e76a5c4da60be36e0808b26173b4b5c44cb1adad
Vladimir <sedohini@yandex.ru>
2022-12-13 19:09:22 +0300

    add jqery, js, carousel section

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,21 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu_item'),
-    hamburger = document.querySelector('.hamburger');
+// "use strict";
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow:'<button type="button" class="slick-prev"><img src="../icons/left.svg"></img></button>',
+        nextArrow:'<button type="button" class="slick-next"><img src="../icons/right.svg"></img></button>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                  dots: true,
+                  arrows:false,
+                }
+            },
 
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
-    });
-
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
-        })
-    })
-})
+          ],
+      });
+  });
